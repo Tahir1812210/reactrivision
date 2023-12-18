3781e986d57dbc29f929e70233f73b75bfde7c81
Tahirid1812210 <cs1812210@szabist.pk>
2023-12-18 22:24:11 +0300

    exercise two is done

--- reactrivision/src/App.tsx
+++ reactrivision/src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import Button from './components/buildingcomponents/exerciseone.tsx/Button';
+import Alert from './components/buildingcomponents/exercisetwo.tsx/Alert';
 
 const App = () => {
-  return <Button onClick={() => console.log('clicked')}>Button</Button>;
+  const [visibility, setVisibility] = useState(false);
+
+  return (
+    <>
+      {visibility && (
+        <Alert onClose={() => setVisibility(false)}>Alert Popped</Alert>
+      )}
+      <Button onClick={() => setVisibility(true)}>Button</Button>
+    </>
+  );
 };
 
 export default App;
---
+++ reactrivision/src/components/buildingcomponents/exercisetwo.tsx/Alert.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  children: string;
+  onClose: () => void;
+}
+
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
