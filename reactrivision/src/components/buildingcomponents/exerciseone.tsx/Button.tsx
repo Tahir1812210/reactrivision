42b600e47169962e6b2ba4c8eec5b2934f0108bf
Tahirid1812210 <cs1812210@szabist.pk>
2023-12-18 19:27:29 +0300

    basic components exercise is done

--- reactrivision/src/components/buildingcomponents/exerciseone.tsx/Button.tsx
+++ reactrivision/src/components/buildingcomponents/exerciseone.tsx/Button.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+interface Props {
+  children: string;
+  color?: string;
+  onClick: () => void;
+}
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ children, onClick, color = 'primary' }: Props) => {
+  return (
+    <button type="button" className={'btn btn-' + color} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
