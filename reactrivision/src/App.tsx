da5a157be4dcddd54a14f217671b3078a97f3dfe
Tahirid1812210 <cs1812210@szabist.pk>
2023-12-20 01:03:28 +0300

    expanded text exercise is now done

--- reactrivision/src/App.tsx
+++ reactrivision/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Button from './components/usingcssmodules/exerciseone.css/Button/Button';
 import Like from './components/usingcssmodules/exercisetwo/Like';
+import ExpandableText from './components/managingcomponentstate/exercisefour/ExpandableText';
 // import Button from './components/buildingcomponents/exerciseone.tsx/Button';
 // import Alert from './components/buildingcomponents/exercisetwo.tsx/Alert';
 
@@ -17,7 +18,21 @@ const App = () => {
 
       {/* usingcssmodules */}
       {/* <Button>Button</Button> */}
-      <Like />
+      {/* <Like /> */}
+
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor at iste
+        aspernatur officia, ea fugit voluptatem corrupti voluptatum saepe totam
+        error amet atque officiis odit tempore rerum possimus! Exercitationem
+        nulla, eum, rem aliquid vel magni quaerat cum libero impedit autem, et
+        ea labore? Non numquam vitae laboriosam cumque commodi velit placeat.
+        Soluta sequi labore ipsa debitis. Aperiam sunt vel reprehenderit illum
+        quisquam facere, commodi praesentium? Accusantium fugiat quae suscipit,
+        soluta recusandae architecto corporis iure quo vero delectus. Aliquam
+        quod, iste dolorum incidunt commodi magnam quam voluptas sint facilis
+        nihil corporis quos id sapiente cum illum, debitis quibusdam architecto
+        pariatur porro!
+      </ExpandableText>
     </>
   );
 };
